9793f0a7ebe178d677591067999f55abbf7a7d33
Çağla Şen <75044298+caglaSen1@users.noreply.github.com>
2021-05-04 16:31:58 +0300

    Delete gameSalingSystemHw4 directory

--- gameSalingSystemHw4/src/Abstract/DiscoutService.java
+++
@@ -1,9 +0,0 @@
-package Abstract;
-
-import Entities.Campaign;
-import Entities.Game;
-
-public interface DiscoutService {
-	void applied(Game game, Campaign campaign);
-
-}
--- gameSalingSystemHw4/src/Abstract/Logger.java
+++
@@ -1,6 +0,0 @@
-package Abstract;
-
-public interface Logger {
-	void log(String message);
-
-}
